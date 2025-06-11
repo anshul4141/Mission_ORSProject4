11cf1903101fde200aeb6d3f8061eefaa7fb016f
anshul4141 <anshulprajapati691@gmail.com>
2025-06-11 17:08:05 +0530

    done

--- ORSProject-4(4-5)/ORSProject-04/src/main/java/in/co/rays/proj4/model/RoleModel.java
+++ ORSProject-4(4-5)/ORSProject-04/src/main/java/in/co/rays/proj4/model/RoleModel.java
@@ -3,7 +3,9 @@ package in.co.rays.proj4.model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import in.co.rays.proj4.bean.RoleBean;
 import in.co.rays.proj4.util.JDBCDataSource;
 
 public class RoleModel {
@@ -16,7 +18,7 @@ public class RoleModel {
 
 		try {
 			conn = JDBCDataSource.getConnection();
-			PreparedStatement pstmt = conn.prepareStatement("select max(id) from st_user");
+			PreparedStatement pstmt = conn.prepareStatement("select max(id) from st_role");
 			ResultSet rs = pstmt.executeQuery();
 			while (rs.next()) {
 				pk = rs.getInt(1);
@@ -27,6 +29,39 @@ public class RoleModel {
 		}
 
 		return pk + 1;
+
+	}
+
+	public int add(RoleBean bean) throws SQLException {
+
+		Connection conn = null;
+		int id = nextPk();
+		try {
+			conn = JDBCDataSource.getConnection();
+
+			conn.setAutoCommit(false);
+
+			PreparedStatement pstmt = conn.prepareStatement("insert into st_role values(?,?,?,?,?,?,?)");
+
+			pstmt.setInt(1, id);
+			pstmt.setString(2, bean.getName());
+			pstmt.setString(3, bean.getDescription());
+			pstmt.setString(4, bean.getCreatedBy());
+			pstmt.setString(5, bean.getModifiedBy());
+			pstmt.setTimestamp(6, bean.getCreatedDatetime());
+			pstmt.setTimestamp(7, bean.getModifiedDatetime());
+
+			int i = pstmt.executeUpdate();
+
+			conn.commit();
+			System.out.println("data inserted successfully " + i);
+
+		} catch (Exception e) {
+			conn.rollback();
+		} finally {
+			conn.close();
+		}
+		return id;
 
 	}
 
--- ORSProject-4(4-5)/ORSProject-04/src/main/java/in/co/rays/proj4/test/TestRoleModel.java
+++ ORSProject-4(4-5)/ORSProject-04/src/main/java/in/co/rays/proj4/test/TestRoleModel.java
@@ -1,11 +1,33 @@
 package in.co.rays.proj4.test;
 
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Date;
+
+import in.co.rays.proj4.bean.RoleBean;
 import in.co.rays.proj4.model.RoleModel;
 
 public class TestRoleModel {
 
-	public static void main(String[] args) {
-		testNextPk();
+	public static void main(String[] args) throws SQLException {
+		// testNextPk();
+		testAdd();
+	}
+
+	private static void testAdd() throws SQLException {
+
+		RoleModel model = new RoleModel();
+
+		RoleBean bean = new RoleBean();
+		bean.setName("admin");
+		bean.setDescription("admin has full access of application");
+		bean.setCreatedBy("root");
+		bean.setModifiedBy("root");
+		bean.setCreatedDatetime(new Timestamp(new Date().getTime()));
+		bean.setModifiedDatetime(new Timestamp(new Date().getTime()));
+
+		model.add(bean);
+
 	}
 
 	private static void testNextPk() {
