ec4b3eb4630b1bf7950adfc05315c76bac3d9f18
anshul4141 <anshulprajapati691@gmail.com>
2026-08-13 12:59:05 +0530

    done

--- Project4-Batch-01-02/ORSProject04/src/main/java/com/sunilos/p4/exception/ApplicationException.java
+++
@@ -1,8 +0,0 @@
-package com.sunilos.p4.exception;
-
-public class ApplicationException extends RuntimeException {
-
-	public ApplicationException(String msg) {
-		super(msg);
-	}
-}
--- Project4-Batch-01-02/ORSProject04/src/main/java/com/sunilos/p4/exception/RecordNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.sunilos.p4.exception;
-
-public class RecordNotFoundException extends RuntimeException {
-
-	public RecordNotFoundException(String msg) {
-		super(msg);
-
-	}
-}
