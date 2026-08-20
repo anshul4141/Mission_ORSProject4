7cae429d18b67f6005371d524fadc171bf1e65d3
anshul4141 <anshulprajapati691@gmail.com>
2026-08-20 11:43:02 +0530

    done

--- Project4-Batch-12-01/ORSProject-04/src/main/java/in/co/rays/proj4/model/RoleModel.java
+++ Project4-Batch-12-01/ORSProject-04/src/main/java/in/co/rays/proj4/model/RoleModel.java
@@ -2,6 +2,7 @@ package in.co.rays.proj4.model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import in.co.rays.proj4.bean.RoleBean;
 import in.co.rays.proj4.exception.ApplicationException;
@@ -31,7 +32,7 @@ public class RoleModel extends BaseModel<RoleBean> {
 			pstmt.executeUpdate();
 			conn.commit();
 
-		} catch (Exception e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 			JDBCDataSource.trnRollBack(conn);
 		} finally {
@@ -44,6 +45,31 @@ public class RoleModel extends BaseModel<RoleBean> {
 	@Override
 	public void update(RoleBean bean) throws ApplicationException, DuplicateRecordException {
 
+		Connection conn = null;
+
+		try {
+			conn = JDBCDataSource.getConnection();
+			conn.setAutoCommit(false); // Begin transaction
+
+			PreparedStatement pstmt = conn.prepareStatement("update " + getTable()
+					+ " set name = ?, description = ?, createdBy = ?, modifiedBy = ?, createdDatetime = ?, modifiedDatetime = ? where id = ?");
+			pstmt.setString(1, bean.getName());
+			pstmt.setString(2, bean.getDescription());
+			pstmt.setString(3, bean.getCreatedBy());
+			pstmt.setString(4, bean.getModifiedBy());
+			pstmt.setTimestamp(5, bean.getCreatedDatetime());
+			pstmt.setTimestamp(6, bean.getModifiedDatetime());
+			pstmt.setLong(7, bean.getId());
+			pstmt.executeUpdate();
+
+			conn.commit(); // End transaction
+			pstmt.close();
+
+		} catch (SQLException e) {
+			JDBCDataSource.trnRollBack(conn);
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
 	}
 
 	@Override
@@ -58,7 +84,7 @@ public class RoleModel extends BaseModel<RoleBean> {
 
 	@Override
 	public RoleBean getBean() {
-		return null;
+		return new RoleBean();
 	}
 
 }
